ceef15faba549ff787ddaf9074cf992cf43b918e
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-26 05:25:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(string)
  end
  return reversed.join(" ")
 end
+
+
+def reverse_each_word(string)
+array = string.split(" ")
+test_array = []
+array.collect do |string|
+test_array << string.reverse
+end
+test_array.join(" ")
+end
