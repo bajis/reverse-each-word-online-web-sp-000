58976c24687302b63699ec586709e104f9bbdcf4
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-26 05:14:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(string)
   split_string = string.split("")
+  reversed_string = []
+
+  string.each do |char|
+  reversed_string.unshift(char)
+ end
+ returnreversed_string.join('')
 end
